25c6d742a822ffbdde687f4f35be681c98b274e0
Maxim Meleshkin <maximmms@hotmail.com>
2024-12-02 16:36:18 +0300

    date_print Added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import telebot
 
+
 token = "7766217558:AAHeBy4VY7wnm-oznPl2exc6epWnXnPJ3j4"
 
 bot = telebot.TeleBot(token)
@@ -16,20 +17,14 @@ HELP = """
 
 tasks = {}
 
-def add_todo(date, task):
+def add_todo(date, task, *category):
     if date in tasks:
-        tasks[date].append(task)
+        tasks.setdefault(date.lower(), []).append((task, category))
     else:
         tasks[date]=[]
-        tasks[date].append(task)
+        tasks.setdefault(date.lower(), []).append((task, category))
 
-
-@bot.message_handler(commands=["help"])
-def help(message):
-    bot.send_message(message.chat.id, HELP)
-
-@bot.message_handler(commands=["add"])
-def add(message):
+def get_command(message):
     command = message.text.split(maxsplit=2)
     date = command[1].lower()
     task = command[2]
@@ -40,9 +35,42 @@ def add(message):
         text = "Задача " + task + " добавлена на дату " + date
         bot.send_message(message.chat.id, text)
 
+def date_print(date,message):
+    if date in tasks:
+        task_ = ""
+        bot.send_message(message.chat.id, f'На дату {date} запланированы задачи:')
+        for task, category in tasks[date]:
+            if category is not None:
+                bot.send_message(message.chat.id, f'{task.upper()}: @{category[0]}')
+            else:
+                bot.send_message(message.chat.id, f'{task.upper()}')
+    else:
+        bot.send_message(message.chat.id, f'На дату {date} нет запланированных задач')
+
+@bot.message_handler(commands=["help"])
+def help(message):
+    bot.send_message(message.chat.id, HELP)
+
+@bot.message_handler(commands=["add"])
+def add(message):
+    command = message.text.split(maxsplit=2)
+    task = command[2].lower()
+    if len(task) > 3:
+        if '@' in task:
+            task_parts = task.split('@')
+            task = task_parts[0].strip()
+            category = task_parts[1].strip() if len(task_parts) > 1 else None
+            add_todo(command[1], task, category)
+            bot.send_message(message.chat.id, f'Задача {task.upper()} добавлена на дату: {command[1]} в категорию - {category.upper()}')
+        else:
+            add_todo(command[1], task)
+            bot.send_message(message.chat.id, f'Задача {task.upper()} добавлена на дату: {command[1]}')
+    else:
+        bot.send_message(message.chat.id, "!!!Задача не может быть короче 3 символов!!!")
+
 @bot.message_handler(commands=["random"])
 def random_add(message):
-    date = "cегодня"
+    date = "Сегодня"
     task = random.choice(RANDOM_TASKS)
     add_todo(date, task)
     text = "Задача " + task + " добавлена на дату " + date
@@ -50,26 +78,16 @@ def random_add(message):
 
 @bot.message_handler(commands=["show"])
 def show(message):
-    command = message.text.split(maxsplit=1)
+    command = message.text.split(maxsplit=2)
     date = command[1].lower()
-    text = ""
-    if date in tasks:
-        text = date.upper() + "\n"
-        for task in tasks[date]:
-            text = text + "[] " + task + "\n"
-    else:
-        text = "Задач на эту дату нет"
-    bot.send_message(message.chat.id, text)
+    date_print(date, message)
 
 @bot.message_handler(commands=["print"])
 def print_(message):
     command = message.text.split()
     dates = command[1:]
     for date in dates:
-        if date in tasks:
-            bot.send_message(message.chat.id, f"На дату {date} установлены задачи: {', '.join(tasks[date])}")
-        else:
-            bot.send_message(message.chat.id, f"На дату {date} задач нет!")
+        date_print(date, message)
 
 @bot.message_handler(content_types=["text"])
 def echo(message):
